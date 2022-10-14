f6c4fae361e9eedd1403b8ca305a94c572ac3f72
Lily <ngross1015@gmail.com>
2022-10-14 13:47:44 -0400

    Complete Connection Routing

--- Star Wars Fans United/routes/connectionRoutes.js
+++ Star Wars Fans United/routes/connectionRoutes.js
@@ -5,4 +5,22 @@ const router = express.Router();
 //GET /connections - get a list of all connections
 router.get('/', controller.index);
 
+//GET /connections/new - send form to create a new connection
+router.get('/new', controller.new);
+
+//POST /connections - post a new connection to the site
+router.post('/', controller.create);
+
+//GET /connections/:id - send the connection with the specified ID
+router.get('/:id', controller.details);
+
+//GET /connections/:id/edit - send form to edit a specified connection
+router.get('/:id/edit', controller.edit);
+
+//PUT /connections/:id - update the connection with the given ID
+router.put('/:id', controller.update);
+
+//DELETE /connections/:id - delete the specified connection
+router.delete('/:id', controller.delete);
+
 module.exports = router;
