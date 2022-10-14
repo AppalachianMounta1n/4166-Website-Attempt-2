fc088311704de63cf998128fe6d60464a7fb0dd6
Lily <ngross1015@gmail.com>
2022-10-14 14:51:51 -0400

    Updated packages and connection access

--- Star Wars Fans United/models/connection.js
+++ Star Wars Fans United/models/connection.js
@@ -21,7 +21,8 @@ const connections = [
         host: 'Lily Gross',
         date: DateTime.now().toLocaleString(DateTime.DATE_MED),
         startTime: '10:00 AM',
-        endTime: '11:00 AM'
+        endTime: '11:00 AM',
+        image: '../public/images/StarWars.png'
     },
     {
         id: uuidv4(),
@@ -31,7 +32,8 @@ const connections = [
         host: 'Lily Gross',
         date: DateTime.now().toLocaleString(DateTime.DATE_MED),
         startTime: '12:00 PM',
-        endTime: '1:00 PM'
+        endTime: '1:00 PM',
+        image: '../public/images/StarWars.png'
     },
     {
         id: uuidv4(),
@@ -41,7 +43,8 @@ const connections = [
         host: 'Lily Gross',
         date: DateTime.now().toLocaleString(DateTime.DATE_MED),
         startTime: '2:00 PM',
-        endTime: '3:00 PM'
+        endTime: '3:00 PM',
+        image: '../public/images/StarWars.png'
     },
     {
         id: uuidv4(),
@@ -51,7 +54,8 @@ const connections = [
         host: 'Lily Gross',
         date: DateTime.now().toLocaleString(DateTime.DATE_MED),
         startTime: '4:00 PM',
-        endTime: '5:00 PM'
+        endTime: '5:00 PM',
+        image: '../public/images/StarWars.png'
     },
     {
         id: uuidv4(),
@@ -61,7 +65,8 @@ const connections = [
         host: 'Lily Gross',
         date: DateTime.now().toLocaleString(DateTime.DATE_MED),
         startTime: '5:30 PM',
-        endTime: '6:30 PM'
+        endTime: '6:30 PM',
+        image: '../public/images/StarWars.png'
     }
 ];
 
